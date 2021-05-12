c1b27ba42e574d49970ff14ef83689812dc2facd
sowmya1412 <soumyakamuni100@gmail.com>
2021-05-12 16:14:55 +0530

    mail

---
+++ mail/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    $('#header-toggler').click(function() {
+        $("#sidebar").toggleClass('sidebar-minimized');
+        $("#sidebar").toggleClass('sidebar-show');
+        $("#overlay").toggleClass('active');
+    });
+    $('#overlay').click(function() {
+        $("#overlay").removeClass('active');
+        $("#sidebar").toggleClass('sidebar-show');
+    });
+
+
+});   
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+     $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+     $(document).ready(function() {
+    $('#student-table').DataTable({
+        lengthChange: true,
+    });
+});
+     $(document).ready(function () {
+$('#sessions').DataTable({
+    processing: true,
+    deferRender: true,
+    pageLength: 10,
+});
+});
+     
